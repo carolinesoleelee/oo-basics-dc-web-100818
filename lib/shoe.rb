b09ad7cb8d95e350fc31cbbf23b50ca319b71296
Solee Lee <carolinesoleelee@gmail.com>
2018-10-10 10:19:11 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+     @brand = brand
+  end
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+  def cobble= cobble
+    @cobble = cobble
+    @brand = brand
+    @condition = condition
+  end
+  def cobble
+    @cobble = puts "Your shoe is as good as new!"
+    @brand
+    @condition = "new"
+  end
+end
